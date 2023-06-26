03d90d17ebd68c1764b3c9985487d784bfcedc0c
SardanaMir <sardanadord@yandex.ru>
2023-06-26 18:55:08 +0300

    second and third pages
    with bem, grid

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+//гамбургер, отображение меню и закрытие меню
+
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    close = document.querySelector('.menu__close'),
+    closeElem = document.querySelector('.menu__overlay');
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+
